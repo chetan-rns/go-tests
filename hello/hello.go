dbc1b037be5c581d01436ab5b33812c1e7f95a91
chetan-rns <chetanrns1997@gmail.com>
2020-03-29 20:27:38 +0530

    Add test for hello world

---
+++ hello/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+const (
+	englishHelloPrefix = "Hello, "
+	kannadaHelloPrefix = "Namaskara, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func hello(name, language string) string {
+	if name == "" {
+		return "Hello, World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	switch language {
+	case Kannada:
+		prefix = kannadaHelloPrefix
+	case Spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(hello("World", "English"))
+}
---
+++ hello/hello_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	t.Run("say hello to people", func(t *testing.T) {
+		got := hello("Foo", "English")
+		want := "Hello, Foo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+		got := hello("", "English")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("add prefix according to language", func(t *testing.T) {
+		got := hello("Foo", "Kannada")
+		want := "Namaskara, Foo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
